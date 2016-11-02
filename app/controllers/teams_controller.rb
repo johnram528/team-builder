8745231d6de2bde0b8f3917a959826629535f243
johnram528 <johnram528@gmail.com>
2016-11-02 15:08:33 -0400

    /teams edit

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,5 @@
 class TeamsController < ApplicationController
     get '/teams' do 
-      if !logged_in?
-        redirect '/login'
-      else
-        @team = Team.find(session[:user_id])
-        erb :'/teams/index'
-      end
+      erb :'/teams/index'
   end
 end

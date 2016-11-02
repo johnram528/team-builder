5a210d7576bf052a4ae5a55316898b41404de42a
johnram528 <johnram528@gmail.com>
2016-11-02 16:43:56 -0400

    added teams_show view and teams/:id route

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,4 +3,9 @@ class TeamsController < ApplicationController
       @teams = Team.all
       erb :'/teams/index'
   end
+
+  get '/teams/:id' do
+    @team = Team.find(params[:id]) 
+    erb :'/teams/team_show'
+  end
 end

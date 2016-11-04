0a9fe58836ec31113f95a38091615b9ad90e4acb
johnram528 <johnram528@gmail.com>
2016-11-04 00:45:33 -0400

    created starters and bench methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,12 @@
 class Team < ActiveRecord::Base
   has_one :coach
   has_many :players
+
+  def starters
+    self.players.all.where(depth: 1)
+  end
+
+  def bench
+    self.players.all.where(depth: 2)
+  end
 end
